25d3a8ed36fa268e8977239f83d23e1963fee2f4
tommib <tommib@03TBj-1117.masino.local>
2018-09-26 14:13:02 +0300

    Base done

---
+++ src/main/java/com/mycompany/juurivuohi/controllers/IndexController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.juurivuohi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author tommib
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String showHome(){
+        return "index";
+    }
+    
+    @RequestMapping("/about")
+    public String showAbout(){
+        return "about";
+    }
+    
+    @RequestMapping("/cvdb")
+    public String showDb(){
+        return "cvdb";
+    }
+    
+    @RequestMapping("/login")
+    public String showLogin(){
+        return "login";
+    }
+    
+}

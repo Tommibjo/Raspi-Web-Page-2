6494448968149abec4164f8f5e8495fb120a6b16
Tommi <tommi.bjrklund@gmail.com>
2018-10-03 22:06:26 +0300

    comment section centered

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -44,7 +44,7 @@ function updateJsonDiv() {
         document.getElementById("timeSpan").appendChild(timeStyle);
 
         var containerStyle = document.createElement("style"); // Luodaan uusi tyyli span osiolle
-        containerStyle.innerHTML = "#mainSpan {border: 1px solid #dedede;background-color: #f1f1f1; padding-top: 5px;padding-bottom:5px;padding-right:7px;padding-left:7px; display: table; width: 405px;margin: 10px 0;}";
+        containerStyle.innerHTML = "#mainSpan {border: 1px solid #dedede;background-color: #f1f1f1; padding-top: 5px;padding-bottom:5px;padding-right:7px;padding-left:7px; display: table; width: 405px;margin: 10px 0;margin-right: auto; margin-left: auto;}";
         document.getElementById("mainSpan").appendChild(containerStyle);
     }
     messages = [];
